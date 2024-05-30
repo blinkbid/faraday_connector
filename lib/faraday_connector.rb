a33fe812bcfcff502472898e45e14fe6ee73b9d2
Rafael Macedo <macedo@87labs.com>
2024-05-30 13:07:02 -0300

    initialize basic ruby gem

---
+++ lib/faraday_connector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "faraday_connector/version"
+
+module FaradayConnector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/faraday_connector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FaradayConnector
+  VERSION = "0.1.0"
+end
